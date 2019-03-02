f502b1f735428a8a5aa1cb793698945b1a96ecf1
Martin Kolev <33600914+Martinkolevbg@users.noreply.github.com>
2019-03-02 12:45:40 +0200

    updated L2_T1

--- F87992_L2_T1.py
+++ F87992_L2_T1.py
@@ -1,10 +1,12 @@
 # F87992_L2_T1.py
 import sys 
 
-argumentList = sys.argv 
+argument = sys.argv[1:]
 
-if sorted(argumentList[1:]) == argumentList[1:] : print ("sorted")
-else : print("unsorted")
-
-
-
+if sorted(argument[0]) >= '9' :
+    argumentint = [int(i) for i in argument]
+    if sorted(argumentint) == argumentint : print ("sorted")
+    else : print("unsorted")
+else :
+    if sorted(argument) == argument : print ("sorted")
+    else : print("unsorted")
